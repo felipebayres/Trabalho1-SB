427026416839e5d40fe1e15569589eb30d5cc739
Felipe <felipebayres@gmail.com>
2019-09-23 15:47:18 -0300

    Funcao de retirar espaco

--- include/PreProcessamento.hpp
+++ include/PreProcessamento.hpp
@@ -1,3 +1,4 @@
 #include <bits/stdc++.h>
 using namespace std;
 void PreProcessamento(string NomeArquivo);
+string RetiraComentarios(string linha);
--- src/PreProcessamento.cpp
+++ src/PreProcessamento.cpp
@@ -5,11 +5,43 @@ using namespace std;
 void PreProcessamento(string NomeArquivo){
     ifstream Arquivo;
     Arquivo.open(NomeArquivo);
-    
+    string linha;
     if (!Arquivo.is_open()){
         cout << "Não foi possível abrir o arquivo " << NomeArquivo << "\n";
         return;
     }
-
-
+    while(getline(Arquivo, linha)){
+        linha = RetiraComentarios(linha);
+        cout << linha << endl;
+    }
 }
+
+
+
+
+
+
+
+
+
+string RetiraComentarios(string linha){
+    int i = 0,posicao = 0,AchouComentario = 0,QuantidadeApagar = 0;
+    
+    while(i < linha.size()){
+        if(linha[i] == ';'){
+            posicao = i;
+            AchouComentario = 1;
+            break;
+        }
+        i++;
+    }
+    QuantidadeApagar = linha.size() - posicao;
+    if (AchouComentario == 1){
+        linha.erase(posicao,QuantidadeApagar);
+    }
+
+
+    return linha;
+}
+
+
