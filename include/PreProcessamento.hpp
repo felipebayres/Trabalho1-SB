905ac9e2c6f00dd0b9a71d074641874efae7e88f
Vítor Ribas <bsbribas66@gmail.com>
2019-09-24 16:19:54 -0300

    adding include PreProcessamento

--- include/PreProcessamento.hpp
+++ include/PreProcessamento.hpp
@@ -7,10 +7,9 @@
 #include <bits/stdc++.h> 
 using namespace std;
 string PreProcessamento(string NomeArquivo);
-
+string TransformaMaiusculo(string linha);
 string RetiraComentarios(string linha);
 string RetiraEspacos(string linha);
-
 int VerificaDiretiva(string linha);
 void DiretivasEQU(string linha);
 bool DiretivasIF(string linha);
