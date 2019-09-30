92f24a0f9a80c63598b939ef24bd0b5135ea13dd
Vítor Ribas <bsbribas66@gmail.com>
2019-09-30 00:06:52 -0300

    basic simulator structure

---
+++ src/Simulador.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#include "Simulador.hpp"
+
+using namespace std;
+
+void Simulador(string linha){
+
+  switch (mnemonico){
+    case ADD:
+      /* code */
+      break;
+    
+    case SUB:
+      /* code */
+      break;
+    
+    case MUL:
+      /* code */
+      break;
+    
+    case DIV:
+      /* code */
+      break;
+    
+    case JMP:
+      /* code */
+      break;
+    
+    case JMPN:
+      /* code */
+      break;
+    
+    case JMPP:
+      /* code */
+      break;
+    
+    case JMPZ:
+      /* code */
+      break;
+    
+    case COPY:
+      /* code */
+      break;
+    
+    case LOAD:
+      /* code */
+      break;
+    
+    case STORE:
+      /* code */
+      break;
+    
+    case INPUT:
+      /* code */
+      break;
+    
+    case OUTPUT:
+      /* code */
+      break;
+    
+    case STOP:
+      /* code */
+      break;
+    
+    default:
+      break;
+  }
+}
