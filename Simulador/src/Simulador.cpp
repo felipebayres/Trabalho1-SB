763776a87db9d28d3db2f9d9b7c435b4165657b5
Vítor Ribas <bsbribas66@gmail.com>
2019-10-17 10:17:04 -0300

    int vector for objects codes working

--- Simulador/src/Simulador.cpp
+++ Simulador/src/Simulador.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 #include <fstream>
 #include <stdint.h>
-#include <string.h>
+#include <string>
+#include <sstream>
 #include "Simulador.hpp"
 using namespace std;
 
@@ -10,8 +11,7 @@ void Simulador(string NomeArqObjeto){
 
   ifstream Arquivo; //ponteiro arquivo
   string linha; // linha do arquivo objeto
-  //vector<string> linha_objeto; // vector para códigos (char)
-  vector<int> int_linha_objeto; // vector para códigos (int)
+  vector<int> vectorObjeto; // vector para códigos (int)
 
   int contador_posicao = 0; // PC
   int linker; // auxiliar para saltos
@@ -25,60 +25,58 @@ void Simulador(string NomeArqObjeto){
   }else{
     getline(Arquivo, linha);
     istringstream iss(linha);
+    string subs;
+    int subs_int;
     while (iss)
     {
-      string subs;
-      int subs_int;
       iss >> subs;
-      //cout << subs << endl;
-      //linha_objeto.push_back(subs);
       if (subs != "OO"){
         subs_int = stoi(subs);
-        int_linha_objeto.push_back(subs_int);
+        vectorObjeto.push_back(subs_int);
       }else{
         continue;
       }
     }
-    for (int i = 0; i < (int_linha_objeto.size()); i++){
-      cout << int_linha_objeto[i];
+    for (int i = 0; i < (vectorObjeto.size()); i++){
+      cout << vectorObjeto[i];
     }
   }
 
-  //while(int_linha_objeto.at(contador_posicao) != STOP){
-  //  switch (int_linha_objeto.at(contador_posicao)){
+  //while(vectorObjeto.at(contador_posicao) != STOP){
+  //  switch (vectorObjeto.at(contador_posicao)){
   //    case ADD:
-  //      ACC = ACC + int_linha_objeto.at(int_linha_objeto.at(++contador_posicao));
+  //      ACC = ACC + vectorObjeto.at(vectorObjeto.at(++contador_posicao));
   //      contador_posicao++;
   //      printf("Valor do acumulador: %d\n", ACC);
   //      break;
   //    
   //    case SUB:
-  //      ACC = ACC - int_linha_objeto.at(int_linha_objeto.at(++contador_posicao));
+  //      ACC = ACC - vectorObjeto.at(vectorObjeto.at(++contador_posicao));
   //      contador_posicao++;
   //      printf("Valor do acumulador: %d\n", ACC);
   //      break;
   //    
   //    case MUL:
-  //      ACC = ACC * int_linha_objeto.at(int_linha_objeto.at(++contador_posicao));
+  //      ACC = ACC * vectorObjeto.at(vectorObjeto.at(++contador_posicao));
   //      contador_posicao++;
   //      printf("Valor do acumulador: %d\n", ACC);
   //      break;
   //    
   //    case DIV:
-  //      ACC = ACC / int_linha_objeto.at(int_linha_objeto.at(++contador_posicao));
+  //      ACC = ACC / vectorObjeto.at(vectorObjeto.at(++contador_posicao));
   //      contador_posicao++;
   //      printf("Valor do acumulador: %d\n", ACC);
   //      break;
   //    
   //    case JMP:
-  //      linker = int_linha_objeto.at(++contador_posicao);
+  //      linker = vectorObjeto.at(++contador_posicao);
   //      contador_posicao = linker;
   //      printf("Valor do acumulador: %d\n", ACC);
   //      break;
   //    
   //    case JMPN:
   //      if(ACC < 0){
-  //        linker = int_linha_objeto.at(++contador_posicao);
+  //        linker = vectorObjeto.at(++contador_posicao);
   //        contador_posicao = linker;
   //      }else{
   //        contador_posicao = contador_posicao + 2;
@@ -88,7 +86,7 @@ void Simulador(string NomeArqObjeto){
   //    
   //    case JMPP:
   //      if(ACC > 0){
-  //        linker = int_linha_objeto.at(++contador_posicao);
+  //        linker = vectorObjeto.at(++contador_posicao);
   //        contador_posicao = linker;
   //      }else{
   //        contador_posicao = contador_posicao + 2;
@@ -98,7 +96,7 @@ void Simulador(string NomeArqObjeto){
   //    
   //    case JMPZ:
   //      if(ACC == 0){
-  //        linker = int_linha_objeto.at(++contador_posicao);
+  //        linker = vectorObjeto.at(++contador_posicao);
   //        contador_posicao = linker;
   //      }else{
   //        contador_posicao = contador_posicao + 2;
@@ -107,9 +105,9 @@ void Simulador(string NomeArqObjeto){
   //      break;
   //    
   //    case COPY:
-  //      int_linha_objeto.at(int_linha_objeto.at(contador_posicao + 2)) = int_linha_objeto.at(int_linha_objeto.at(contador_posicao + 1));
-  //      memoria[0] = int_linha_objeto.at(contador_posicao + 2);
-  //      memoria[1] = int_linha_objeto.at(int_linha_objeto.at(contador_posicao + 1));
+  //      vectorObjeto.at(vectorObjeto.at(contador_posicao + 2)) = vectorObjeto.at(vectorObjeto.at(contador_posicao + 1));
+  //      memoria[0] = vectorObjeto.at(contador_posicao + 2);
+  //      memoria[1] = vectorObjeto.at(vectorObjeto.at(contador_posicao + 1));
   //      contador_posicao = contador_posicao + 3;
 //
   //      printf("Endereço de Memória: %d\n", memoria[0]);
@@ -118,14 +116,14 @@ void Simulador(string NomeArqObjeto){
   //      break;
   //    
   //    case LOAD:
-  //      ACC = int_linha_objeto.at(int_linha_objeto.at(++contador_posicao));
+  //      ACC = vectorObjeto.at(vectorObjeto.at(++contador_posicao));
   //      contador_posicao++;
   //      printf("Valor do acumulador: %d\n", ACC);
   //      break;
   //    
   //    case STORE:
-  //      int_linha_objeto.at(int_linha_objeto.at(++contador_posicao)) = ACC;
-  //      memoria[0] = int_linha_objeto.at(contador_posicao);
+  //      vectorObjeto.at(vectorObjeto.at(++contador_posicao)) = ACC;
+  //      memoria[0] = vectorObjeto.at(contador_posicao);
   //      memoria[1] = ACC;
   //      contador_posicao++;
   //      break;
@@ -133,8 +131,8 @@ void Simulador(string NomeArqObjeto){
   //    case INPUT:
   //      cout << "Favor inserir um valor numérico: ";
   //      cin >> linker;
-  //      int_linha_objeto.at(int_linha_objeto.at(++contador_posicao)) = linker;
-  //      memoria[0] = int_linha_objeto.at(contador_posicao);
+  //      vectorObjeto.at(vectorObjeto.at(++contador_posicao)) = linker;
+  //      memoria[0] = vectorObjeto.at(contador_posicao);
   //      memoria[1] = linker;
   //      contador_posicao++;
 //
@@ -143,7 +141,7 @@ void Simulador(string NomeArqObjeto){
   //      break;
 //
   //    case OUTPUT:
-  //      cout << "O valor de saída é:" << int_linha_objeto.at(int_linha_objeto.at(++contador_posicao)) << endl;
+  //      cout << "O valor de saída é:" << vectorObjeto.at(vectorObjeto.at(++contador_posicao)) << endl;
   //      contador_posicao++;
   //      printf("Valor do acumulador: %d\n", ACC);
   //      break;
