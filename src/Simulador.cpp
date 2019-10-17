50cd950bc29a39f48d608eb0a39b750a87e24928
Vítor Ribas <bsbribas66@gmail.com>
2019-10-17 08:16:54 -0300

    new logic for get codes from line in object file

--- src/Simulador.cpp
+++ src/Simulador.cpp
@@ -10,151 +10,146 @@ void Simulador(string NomeArqObjeto){
 
   ifstream Arquivo; //ponteiro arquivo
   string linha;
-  string espaco = " ";
-  vector<int> linha_objeto; // linha do arquivo objeto
-  int codigo_int;
-  char codigo_char[2] = {'\0', '\0'};
-  char codigo[3] = {'\0', '\0', '\0'};
+  vector<string> linha_objeto; // linha do arquivo objeto
+  vector<int> int_linha_objeto;
+
   int contador_posicao = 0; // PC
   int linker; // auxiliar para saltos
   int ACC = 0;  // registrador acumulador
   int16_t memoria[2]; // memoria simulada
 
   Arquivo.open(NomeArqObjeto);
-    
   if (!Arquivo.is_open()){
     cout << "Não foi possível abrir o arquivo objeto " << NomeArqObjeto << "\n";
     return ;
   }else{
-    while(!Arquivo.eof())
+    getline(Arquivo, linha);
+    istringstream iss(linha);
+    while (iss)
     {
-      Arquivo.read(codigo_char, 1);
-      if (!Arquivo.eof()){
-        if(espaco.compare(codigo_char)){   // se não for espaço
-          codigo[0] = codigo_char[0];      // primeiro dígito do codigo
-          Arquivo.read(codigo_char, 1);    // lẽ próxima posição
-          if(!espaco.compare(codigo_char) || Arquivo.eof()){  // se for espaço ou fim do arquivo
-            codigo_int = stoi(string(codigo));  // finaliza o codigo convertendo pra int
-						linha_objeto.push_back(codigo_int); // põem no vetor de códigos objetos
-          } else if(!Arquivo.eof()){  // se nao for fim do arquivo
-            codigo[1] = codigo_char[0]; // completo o codigo com seu segundo dígito
-						codigo_int = stoi(string(codigo));  // converte pra int
-						linha_objeto.push_back(codigo_int); // põem no vetor de códigos objetos
-          }
-        }else{
-          continue;
-        }
+      string subs;
+      int subs_int;
+      iss >> subs;
+      //cout << subs << endl;
+      linha_objeto.push_back(subs);
+      if (subs != "OO"){
+        subs_int = stoi(subs);
+        int_linha_objeto.push_back(subs_int);
+      }else{
+        continue;
       }
     }
-  }
-
-  while(linha_objeto.at(contador_posicao) != STOP){
-
-    switch (linha_objeto.at(contador_posicao)){
-      case ADD:
-        ACC = ACC + linha_objeto.at(linha_objeto.at(++contador_posicao));
-        contador_posicao++;
-        printf("Valor do acumulador: %d\n", ACC);
-        break;
-      
-      case SUB:
-        ACC = ACC - linha_objeto.at(linha_objeto.at(++contador_posicao));
-        contador_posicao++;
-        printf("Valor do acumulador: %d\n", ACC);
-        break;
-      
-      case MUL:
-        ACC = ACC * linha_objeto.at(linha_objeto.at(++contador_posicao));
-        contador_posicao++;
-        printf("Valor do acumulador: %d\n", ACC);
-        break;
-      
-      case DIV:
-        ACC = ACC / linha_objeto.at(linha_objeto.at(++contador_posicao));
-        contador_posicao++;
-        printf("Valor do acumulador: %d\n", ACC);
-        break;
-      
-      case JMP:
-        linker = linha_objeto.at(++contador_posicao);
-        contador_posicao = linker;
-        printf("Valor do acumulador: %d\n", ACC);
-        break;
-      
-      case JMPN:
-        if(ACC < 0){
-          linker = linha_objeto.at(++contador_posicao);
-          contador_posicao = linker;
-        }else{
-          contador_posicao = contador_posicao + 2;
-        }
-        printf("Valor do acumulador: %d\n", ACC);
-        break;
-      
-      case JMPP:
-        if(ACC > 0){
-          linker = linha_objeto.at(++contador_posicao);
-          contador_posicao = linker;
-        }else{
-          contador_posicao = contador_posicao + 2;
-        }
-        printf("Valor do acumulador: %d\n", ACC);
-        break;
-      
-      case JMPZ:
-        if(ACC == 0){
-          linker = linha_objeto.at(++contador_posicao);
-          contador_posicao = linker;
-        }else{
-          contador_posicao = contador_posicao + 2;
-        }
-        printf("Valor do acumulador: %d\n", ACC);
-        break;
-      
-      case COPY:
-        linha_objeto.at(linha_objeto.at(contador_posicao + 2)) = linha_objeto.at(linha_objeto.at(contador_posicao + 1));
-        memoria[0] = linha_objeto.at(contador_posicao + 2);
-        memoria[1] = linha_objeto.at(linha_objeto.at(contador_posicao + 1));
-        contador_posicao = contador_posicao + 3;
-
-        printf("Endereço de Memória: %d\n", memoria[0]);
-        printf("Valor de Memória: %d\n", memoria[1]);
-
-        break;
-      
-      case LOAD:
-        ACC = linha_objeto.at(linha_objeto.at(++contador_posicao));
-        contador_posicao++;
-        printf("Valor do acumulador: %d\n", ACC);
-        break;
-      
-      case STORE:
-        linha_objeto.at(linha_objeto.at(++contador_posicao)) = ACC;
-        memoria[0] = linha_objeto.at(contador_posicao);
-        memoria[1] = ACC;
-        contador_posicao++;
-        break;
-
-      case INPUT:
-        cout << "Favor inserir um valor numérico: ";
-        cin >> linker;
-        linha_objeto.at(linha_objeto.at(++contador_posicao)) = linker;
-        memoria[0] = linha_objeto.at(contador_posicao);
-        memoria[1] = linker;
-        contador_posicao++;
-
-        printf("Endereço de Memória: %d\n", memoria[0]);
-        printf("Valor de Memória: %d\n", memoria[1]);
-        break;
-
-      case OUTPUT:
-        cout << "O valor de saída é:" << linha_objeto.at(linha_objeto.at(++contador_posicao)) << endl;
-        contador_posicao++;
-        printf("Valor do acumulador: %d\n", ACC);
-        break;
-
-      default:
-        break;
+    for (int i = 0; i < (int_linha_objeto.size()); i++){
+      cout << int_linha_objeto[i];
     }
   }
+
+  //while(int_linha_objeto.at(contador_posicao) != STOP){
+  //  switch (int_linha_objeto.at(contador_posicao)){
+  //    case ADD:
+  //      ACC = ACC + int_linha_objeto.at(int_linha_objeto.at(++contador_posicao));
+  //      contador_posicao++;
+  //      printf("Valor do acumulador: %d\n", ACC);
+  //      break;
+  //    
+  //    case SUB:
+  //      ACC = ACC - int_linha_objeto.at(int_linha_objeto.at(++contador_posicao));
+  //      contador_posicao++;
+  //      printf("Valor do acumulador: %d\n", ACC);
+  //      break;
+  //    
+  //    case MUL:
+  //      ACC = ACC * int_linha_objeto.at(int_linha_objeto.at(++contador_posicao));
+  //      contador_posicao++;
+  //      printf("Valor do acumulador: %d\n", ACC);
+  //      break;
+  //    
+  //    case DIV:
+  //      ACC = ACC / int_linha_objeto.at(int_linha_objeto.at(++contador_posicao));
+  //      contador_posicao++;
+  //      printf("Valor do acumulador: %d\n", ACC);
+  //      break;
+  //    
+  //    case JMP:
+  //      linker = int_linha_objeto.at(++contador_posicao);
+  //      contador_posicao = linker;
+  //      printf("Valor do acumulador: %d\n", ACC);
+  //      break;
+  //    
+  //    case JMPN:
+  //      if(ACC < 0){
+  //        linker = int_linha_objeto.at(++contador_posicao);
+  //        contador_posicao = linker;
+  //      }else{
+  //        contador_posicao = contador_posicao + 2;
+  //      }
+  //      printf("Valor do acumulador: %d\n", ACC);
+  //      break;
+  //    
+  //    case JMPP:
+  //      if(ACC > 0){
+  //        linker = int_linha_objeto.at(++contador_posicao);
+  //        contador_posicao = linker;
+  //      }else{
+  //        contador_posicao = contador_posicao + 2;
+  //      }
+  //      printf("Valor do acumulador: %d\n", ACC);
+  //      break;
+  //    
+  //    case JMPZ:
+  //      if(ACC == 0){
+  //        linker = int_linha_objeto.at(++contador_posicao);
+  //        contador_posicao = linker;
+  //      }else{
+  //        contador_posicao = contador_posicao + 2;
+  //      }
+  //      printf("Valor do acumulador: %d\n", ACC);
+  //      break;
+  //    
+  //    case COPY:
+  //      int_linha_objeto.at(int_linha_objeto.at(contador_posicao + 2)) = int_linha_objeto.at(int_linha_objeto.at(contador_posicao + 1));
+  //      memoria[0] = int_linha_objeto.at(contador_posicao + 2);
+  //      memoria[1] = int_linha_objeto.at(int_linha_objeto.at(contador_posicao + 1));
+  //      contador_posicao = contador_posicao + 3;
+//
+  //      printf("Endereço de Memória: %d\n", memoria[0]);
+  //      printf("Valor de Memória: %d\n", memoria[1]);
+//
+  //      break;
+  //    
+  //    case LOAD:
+  //      ACC = int_linha_objeto.at(int_linha_objeto.at(++contador_posicao));
+  //      contador_posicao++;
+  //      printf("Valor do acumulador: %d\n", ACC);
+  //      break;
+  //    
+  //    case STORE:
+  //      int_linha_objeto.at(int_linha_objeto.at(++contador_posicao)) = ACC;
+  //      memoria[0] = int_linha_objeto.at(contador_posicao);
+  //      memoria[1] = ACC;
+  //      contador_posicao++;
+  //      break;
+//
+  //    case INPUT:
+  //      cout << "Favor inserir um valor numérico: ";
+  //      cin >> linker;
+  //      int_linha_objeto.at(int_linha_objeto.at(++contador_posicao)) = linker;
+  //      memoria[0] = int_linha_objeto.at(contador_posicao);
+  //      memoria[1] = linker;
+  //      contador_posicao++;
+//
+  //      printf("Endereço de Memória: %d\n", memoria[0]);
+  //      printf("Valor de Memória: %d\n", memoria[1]);
+  //      break;
+//
+  //    case OUTPUT:
+  //      cout << "O valor de saída é:" << int_linha_objeto.at(int_linha_objeto.at(++contador_posicao)) << endl;
+  //      contador_posicao++;
+  //      printf("Valor do acumulador: %d\n", ACC);
+  //      break;
+//
+  //    default:
+  //      break;
+  //  }
+  //}
 }
--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int main (int argc, char** argv) {
     string NomeArquivo, NomeArquivoPreProcessado, NomeArquivoMontado;
-    if (argc < 3){
+    if (argc < 2){
         cout << "Insira o nome do arquivo a ser usado!" << "\n";
         return 0;
     }   
